2543d45883e0b4bf00fae64fee06725a39819928
wen-fei <282699766@qq.com>
2017-08-06 01:26:22 +0800

    逻辑回归用于分类

--- Logistic回归/logRegres.py
+++ Logistic回归/logRegres.py
@@ -103,3 +103,47 @@ def stocGradAscent1(dataMatrix, classLabels, numIter=150):
     return weights
 
 
+# 用逻辑回归进行分类
+def classifyVector(inX, weights):
+    prob = sigmoid(np.sum(inX * weights))
+    if prob > 0.5:
+        return 1.0
+    else:
+        return 0.0
+
+
+def colicTest():
+    frTrain = open('horseColicTraining.txt')
+    frTest = open('horseColicTest.txt')
+    trainingSet = []
+    trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    trainWeights = stocGradAscent1(np.array(trainingSet), trainingLabels, 500)
+    errorCount = 0
+    numTestVec = 0.0
+    for line in frTest.readlines():
+        numTestVec += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(np.array(lineArr), trainWeights)) != int(currLine[21]):
+            errorCount += 1
+    errorRate = float(errorCount) / numTestVec
+    print "the error rate of this test is : %f" % errorRate
+    return errorRate
+
+
+def multiTest():
+    numTests = 10
+    errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print "after %d iterations the average error rate is: %f " % (numTests, errorSum / float(numTests))
+
