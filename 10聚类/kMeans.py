09349ffb0e36d3669787fd1f8daf4acbac7988f6
wen-fei <282699766@qq.com>
2017-08-24 23:14:56 +0800

    代码

--- 10聚类/kMeans.py
+++ 10聚类/kMeans.py
@@ -108,3 +108,35 @@ def biKmeans(dataSet, k, distMeas=distEclud):
         )[0], :] = bestClustAss
     print "centList is:", centList
     return np.mat(centList), clusterAssment
+
+
+import urllib
+import json
+def geoGrab(stAddress, city):
+    apiStem = 'http://where.yahooapis.com/geocode?'  #create a dict and constants for the goecoder
+    params = {}
+    params['flags'] = 'J'#JSON return type
+    params['appid'] = 'aaa0VN6k'
+    params['location'] = '%s %s' % (stAddress, city)
+    url_params = urllib.urlencode(params)
+    yahooApi = apiStem + url_params      #print url_params
+    print yahooApi
+    c=urllib.urlopen(yahooApi)
+    return json.loads(c.read())
+
+
+from time import sleep
+def massPlaceFind(fileName):
+    fw = open('places.txt', 'w')
+    for line in open(fileName).readlines():
+        line = line.strip()
+        lineArr = line.split('\t')
+        retDict = geoGrab(lineArr[1], lineArr[2])
+        if retDict['ResultSet']['Error'] == 0:
+            lat = float(retDict['ResultSet']['Results'][0]['latitude'])
+            lng = float(retDict['ResultSet']['Results'][0]['longitude'])
+            print "%s\t%f\t%f" % (lineArr[0], lat, lng)
+            fw.write('%s\t%f\t%f\n' % (line, lat, lng))
+        else: print "error fetching"
+        sleep(1)
+    fw.close()
