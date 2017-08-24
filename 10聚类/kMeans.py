96143feaf6a5958460af6152e9ff569fce5f76ba
wen-fei <282699766@qq.com>
2017-08-24 21:47:46 +0800

    代码

---
+++ 10聚类/kMeans.py
@@ -0,0 +1,110 @@
+# coding=utf-8
+# author ：Landuy
+# time ：2017/8/24
+# email ：qq282699766@gamil.com
+import numpy as np
+
+
+def loadDataSet(fileName):
+    dataMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = map(float, curLine)
+        dataMat.append(fltLine)
+    return dataMat
+
+
+# 计算两个向量的欧式距离
+def distEclud(vecA, vecB):
+    return np.sqrt(np.sum(np.power(vecA - vecB, 2)))
+
+
+# 为给定数据集构建一个包含K个随机质心的集合
+def randCent(dataSet, k):
+    n = np.shape(dataSet)[1]
+    centroids = np.mat(np.zeros((k, n)))
+    for j in range(n):
+        # 构建簇质心
+        # try:
+        minJ = np.min(dataSet[:, j])
+        rangeJ = float(np.max(dataSet[:, j]) - minJ)
+        centroids[:, j] = minJ + rangeJ * np.random.rand(k, 1)
+        #except ValueError:
+        #    pass
+    return centroids
+
+
+# K均值聚类算法
+# @dataSet: 数据集
+# @k : 据点个数
+# @distMeas: 距离计算方法
+# @createCent: 质心选择方法
+def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
+    m = np.shape(dataSet)[0]
+    # 包含两列， 第一列记录簇索引值，第二列存储误差（当前点到质心的距离）
+    clusterAssment = np.mat(np.zeros((m, 2)))
+    centroids = createCent(dataSet, k)
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        for i in range(m):
+            minDist =np.inf
+            minIndex = -1
+            # 寻找最近的质心
+            for j in range(k):
+                distJI = distMeas(centroids[j, :], dataSet[i, :])
+                if distJI < minDist:
+                    minDist = distJI
+                    minIndex = j
+            if clusterAssment[i, 0] != minIndex:
+                clusterChanged = True
+            clusterAssment[i, :] = minIndex, minDist**2
+        print centroids
+        for cent in range(k):
+            ptsInClust = dataSet[np.nonzero(clusterAssment[:, 0].A == cent)[0]]
+            # 遍历所有质心并更新取值
+            centroids[cent, :] = np.mean(ptsInClust, axis=0)
+    return centroids, clusterAssment
+
+
+# 二分K-均值聚类算法
+def biKmeans(dataSet, k, distMeas=distEclud):
+    m = np.shape(dataSet)[0]
+    clusterAssment = np.mat(np.zeros((m, 2)))
+    # 创建一个初始簇
+    centroid0 = np.mean(dataSet, axis=0).tolist()[0]
+    centList = [centroid0]
+    for j in range(m):
+        clusterAssment[j, 1] = distMeas(np.mat(centroid0), dataSet[j, :]) ** 2
+    while len(centList) < k:
+        lowestSSE = np.inf
+        for i in range(len(centList)):
+            # 尝试划分每一簇
+            ptsInCurrCluster = dataSet[np.nonzero(clusterAssment[:, 0].A == i)[0], :]
+            centroidMat, splitClusterAss = kMeans(
+                ptsInCurrCluster, 2, distMeas
+            )
+            sseSplit = np.sum(splitClusterAss[:, 1])
+            sseNotSplit = np.sum(
+                clusterAssment[np.nonzero(clusterAssment[:, 0].A != i)[0], 1]
+            )
+            print "sseSplit, and notSplit : ", sseSplit, sseNotSplit
+            if (sseSplit + sseNotSplit) < lowestSSE:
+                bestCentToSplit = i
+                bestNewCents = centroidMat
+                bestClustAss = splitClusterAss.copy()
+                lowestSSE = sseSplit + sseNotSplit
+        # 更新簇的分配结果
+        bestClustAss[np.nonzero(bestClustAss[:, 0].A == 1)[0], 0] = len(centList)
+        bestClustAss[np.nonzero(bestClustAss[:, 0].A == 0)[0], 0] = bestCentToSplit
+        print 'the bestCentTopSplit is:', bestCentToSplit
+        print 'the len of bestClustAss is:', len(bestClustAss)
+        centList[bestCentToSplit] = bestNewCents[0, :]
+        # 书上代码错误，应该加.tolist()[0]
+        centList.append(bestNewCents[1, :].tolist()[0])
+        clusterAssment[np.nonzero(
+            clusterAssment[:, 0].A == bestCentToSplit
+        )[0], :] = bestClustAss
+    print "centList is:", centList
+    return np.mat(centList), clusterAssment
